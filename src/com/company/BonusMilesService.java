5c179412c06373d52f7487227b103bfd1dbb0acb
Olga Em <olga.em020@gmail.com>
2022-02-25 00:01:03 +0700

    added save

---
+++ src/com/company/BonusMilesService.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int mile = 20;
+        int miles = cost / mile;
+        return miles;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,9 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 16_200;
-        int mile = 20;
-        int miles = ticketPrice / mile;
-        System.out.println("Ваши накопленные мили " + miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
